5b974b5ae794b69a440039a60583d19e51b0c35a
Brendan Jocson <krull.mcroth@gmail.com>
2017-01-10 13:05:48 +0200

    solved race condition at init

--- init_fs/usr/local/bin/docker-entrypoint.sh
+++ init_fs/usr/local/bin/docker-entrypoint.sh
@@ -2,5 +2,11 @@
 
 set -e
 
-echo "Starting the zenoss daemons..."
+while ! /bin/nc -z zenoss4-mariadb 3306;
+  do
+    echo "Waiting for zenoss4-mariadb to initialize...";
+    sleep 1;
+  done;
+
+echo "Dependencies Started.. Starting the zenoss daemons..."
 service zenoss start & tail -f /usr/local/zenoss/log/Z2.log
